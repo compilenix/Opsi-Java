12fb1d6a22d82a2404816e942e5d68c16f854f9a
Compilenix <Compilenix@compilenix.org>
2016-03-30 19:08:43 +0200

    Update to Java 8 update 77

--- shasum.sh
+++ shasum.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-find CLIENT_DATA -type f -exec sha1sum {} \; > sha1sum.txt &
-find CLIENT_DATA -type f -exec sha256sum {} \; > sha256sum.txt &
-wait
-
+find CLIENT_DATA -type f -exec sha256sum {} \; > sha256sum.txt
